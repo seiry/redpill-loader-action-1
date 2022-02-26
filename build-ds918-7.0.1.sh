83537fbdaaae403c3385e2e414803c607d128435
Seiry Yu <4397354+seiry@users.noreply.github.com>
2022-02-26 21:03:02 +0800

    Update build-ds918-7.0.1.sh

--- build-ds918-7.0.1.sh
+++ build-ds918-7.0.1.sh
@@ -28,6 +28,7 @@ cd ..
 cd redpill-load
 cp ${root}/user_config.DS918+.json ./user_config.json
 ./ext-manager.sh add https://raw.githubusercontent.com/jumkey/redpill-load/develop/redpill-acpid/rpext-index.json
+# https://www.openos.org/threads/20220220dsm7-0-1-_esata____ddns.3773/
 sudo ./build-loader.sh 'DS918+' '7.0.1-42218'
 mv images/redpill-DS918+_7.0.1-4221*.img ${root}/output/
 cd ${root}
